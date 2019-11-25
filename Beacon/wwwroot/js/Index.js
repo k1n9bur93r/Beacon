be1d6546e29dfed936a11c7f48f1db9fa7a41684
Dennis Luong <luongd2@unlv.nevada.edu>
2019-11-25 14:52:48 -0800

    Added pictures for landing page

--- Beacon/wwwroot/js/Index.js
+++ Beacon/wwwroot/js/Index.js
@@ -2,9 +2,9 @@ $("#slideshow > div:gt(0)").hide();
 
 setInterval(function () {
     $('#slideshow > div:first')
-        .fadeOut(2000)
+        .fadeOut(3000)
         .next()
-        .fadeIn(2000)
+        .fadeIn(3000)
         .end()
         .appendTo('#slideshow');
-}, 5000);
+}, 6000);
